e25f7fda8f39b9445c259d594f2c4946d88b203f
Blue Swallow <parastoomaleki100@gmail.com>
2025-04-13 20:58:21 +0330

    Initial commit

---
+++ apps/cms-project/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'cms-project',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/cms-project',
+};
