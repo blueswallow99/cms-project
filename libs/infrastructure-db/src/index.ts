ac7202ca3481906e722f9a261f80b6e15a0dc44b
blueswallow <parastoomaleki100@gmail.com>
2025-04-14 17:25:39 +0330

    add index.ts exports of modules

--- libs/infrastructure-db/src/index.ts
+++ libs/infrastructure-db/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/infrastructure-db.module';
+export * from './lib/entities/customer.entity';
+export * from './lib/entities/order.entity';
+export * from './lib/repositories/customer.repository';
+export * from './lib/entities/order-item.entity';
--- libs/order-application/src/lib/order-application.module.ts
+++ libs/order-application/src/lib/order-application.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { UpdateOrderStatusHandler } from './handlers/update-order-status.handler';
+import { ProcessPaymentHandler } from './handlers/process-payment.handler';
+import { DeleteOrderHandler } from './handlers/delete-order.handler';
+import { GetOrderHandler } from './handlers/get-order.handler';
+import { GetCustomerOrdersHandler } from './handlers/get-customer-orders.handler';
+import { OrderMapper } from './mappers/order.mapper';
+import { UpdateOrderStatusCommand } from './commands/update-order-status.command';
+import { ProcessPaymentCommand } from './commands/process-payment.command';
+import { DeleteOrderCommand } from './commands/delete-order.command';
+
+// const CommandHandlers = [
+//   UpdateOrderStatusCommand,
+//   ProcessPaymentCommand,
+//   DeleteOrderCommand,
+// ];
+
+// const QueryHandlers = [
+//   GetOrderHandler,
+//   GetCustomerOrdersHandler,
+// ];
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [CqrsModule],
+  providers: [
+    // ...CommandHandlers,
+    // ...QueryHandlers,
+    OrderMapper,
+  ],
+  exports: [CqrsModule, OrderMapper],
 })
 export class OrderApplicationModule {}
